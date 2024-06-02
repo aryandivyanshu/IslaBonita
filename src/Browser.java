247b6653e8e35cf803b19e379c1d8235409564c9
Aryan Divyanshu <aryandivyanshu33@gmail.com>
2024-06-02 17:54:02 +0530

    v1.1.0

---
+++ src/Browser.java
@@ -0,0 +1,23 @@
+public class Browser {
+
+    // takes in the address as user input and then leads all other methods
+    // and then finally outputs the product html of all the processing.
+    public void navigate(String address) {
+        // this method is the lead method
+        String ip_number = findIPAddress(address);
+        String html = sendHTTPRequest(ip_number);
+        System.out.println(html);
+
+    }
+
+
+        //converts the user input address to ip number looking it up at DNS
+    public String findIPAddress(String address){
+        return "ip number";
+    }
+
+    //takes ip number as input and return html content received
+    public  String sendHTTPRequest(String ip_number){
+        return "<html> <body>HTTP CONTENT</body> </html>";
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("IslaBonita");
+
+        System.out.println("IslaBonita Browser");
+
+        Browser browser = new Browser();
+        browser.navigate("www.example.com");
+
     }
+
+
+
 }

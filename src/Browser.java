93d67acd99fc561fa013a22f3657016f77f8411e
Aryan Divyanshu <aryandivyanshu33@gmail.com>
2024-06-07 16:53:32 +0530

    v.1.1.2 : address to ip address using InetAddress

--- src/Browser.java
+++ src/Browser.java
@@ -1,11 +1,17 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Browser {
 
     // takes in the address as user input and then leads all other methods
     // and then finally outputs the product html of all the processing.
     public void navigate(String address) {
         // this method is the lead method
-        String ip_number = findIPAddress(address);
-        String html = sendHTTPRequest(ip_number);
+
+        String ipAddressVariableAsParameter = findIPAddress(address);
+        //will set the return of findIPAddress method, aka "ipAddress" variable's value as value of variable (ipAddressVariableAsParameter) to be sent as parameter in next method.
+
+        String html = sendHTTPRequest(ipAddressVariableAsParameter);
         System.out.println(html);
 
     }
@@ -13,11 +19,25 @@ public class Browser {
 
         //converts the user input address to ip number looking it up at DNS
     private String findIPAddress(String address){
-        return "ip number";
+        String ipAddress = null;
+
+        try {
+            InetAddress addressObject = InetAddress.getByName(address);
+            ipAddress = addressObject.getHostAddress();
+            System.out.println("IP Address of " + address + " : " + ipAddress);
+        }
+
+        catch (UnknownHostException ex) {
+            System.out.println("Could not resolve domain: " + address);
+            ex.printStackTrace();
+        }
+
+        return ipAddress ;
+
     }
 
     //takes ip number as input and return html content received
-    private String sendHTTPRequest(String ip_number){
+    private String sendHTTPRequest(String ipAddress){
         return "<html> <body>HTTP CONTENT</body> </html>";
     }
 }
